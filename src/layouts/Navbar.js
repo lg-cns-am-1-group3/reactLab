8a15fadc4acd72b556f065c9d33d817c637afa12
letthem <suyeon3662@naver.com>
2025-01-17 15:27:21 +0900

    Feat: 이름 추가

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -21,67 +21,67 @@ export default function Navbar() {
             <Link to="/pokemon-ohhyungsuh">
               <p className="title">포켓몬</p>
             </Link>
-            <p className="developer">000</p>
+            <p className="developer">오형서</p>
           </li>
           <li>
             <Link to="/university">
               <p className="title">대학</p>
             </Link>
-            <p className="developer">000</p>
+            <p className="developer">이다빈</p>
           </li>
           <li>
             <Link to="/tvmaze">
               <p className="title">티비</p>
             </Link>
-            <p className="developer">000</p>
+            <p className="developer">심수연</p>
           </li>
           <li>
             <Link to="/pokemon-poke-shy">
               <p className="title">포켓몬</p>
             </Link>
-            <p className="developer">000</p>
+            <p className="developer">유승희</p>
           </li>
           <li>
             <Link to="/genderize">
               <p className="title">젠더라이즈</p>
             </Link>
-            <p className="developer">000</p>
+            <p className="developer">윤다인</p>
           </li>
           <li>
             <Link to="/newslist-hanjin">
               <p className="title">뉴스리스트</p>
             </Link>
-            <p className="developer">000</p>
+            <p className="developer">양한진</p>
           </li>
           <li>
             <Link to="/libretranslate">
               <p className="title">번역</p>
             </Link>
-            <p className="developer">000</p>
+            <p className="developer">오태양</p>
           </li>
           <li>
             <Link to="/dogbreed">
               <p className="title">견종</p>
             </Link>
-            <p className="developer">000</p>
+            <p className="developer">현민영</p>
           </li>
           <li>
             <Link to="/random">
               <p className="title">랜덤</p>
             </Link>
-            <p className="developer">000</p>
+            <p className="developer">유영서</p>
           </li>
           <li>
             <Link to="/newslist-yebin">
               <p className="title">뉴스리스트</p>
             </Link>
-            <p className="developer">000</p>
+            <p className="developer">신예빈</p>
           </li>
           <li>
             <Link to="/harrypotter">
               <p className="title">해리포터</p>
             </Link>
-            <p className="developer">000</p>
+            <p className="developer">이상훈</p>
           </li>
         </ul>
       </header>
